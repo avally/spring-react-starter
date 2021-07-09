7a288140ac77f64ee5726fed98e41180037282c0
Dmitry Polishchuk <dim.polishchuk@gmail.com>
2021-07-09 20:13:50 +0300

    init SpringBoot project

---
+++ src/main/java/org/avally/springreactstarter/SpringReactStarterApplication.java
@@ -0,0 +1,13 @@
+package org.avally.springreactstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactStarterApplication.class, args);
+    }
+
+}
