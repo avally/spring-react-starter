28397974d50f7df0285152ecbdc339e63eca7938
Dmitry Polishchuk <dim.polishchuk@gmail.com>
2021-07-16 19:52:37 +0300

    no functional changes

--- src/main/java/org/avally/springreactstarter/security/jwt/JwtUtils.java
+++ src/main/java/org/avally/springreactstarter/security/jwt/JwtUtils.java
@@ -29,9 +29,9 @@ public class JwtUtils {
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder()
-                .setSubject((userPrincipal.getUsername()))
+                .setSubject(userPrincipal.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
     }
