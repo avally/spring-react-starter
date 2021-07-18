88667eec19b1aa77597c6ea1893ced2d197a030c
Dmitry Polishchuk <dim.polishchuk@gmail.com>
2021-07-18 06:22:46 +0300

    rewrite JWT authorization

--- src/main/java/org/avally/springreactstarter/controller/AuthController.java
+++ src/main/java/org/avally/springreactstarter/controller/AuthController.java
@@ -1,71 +1,70 @@
 package org.avally.springreactstarter.controller;
 
-import org.avally.springreactstarter.model.ERole;
 import org.avally.springreactstarter.model.Role;
+import org.avally.springreactstarter.model.Status;
 import org.avally.springreactstarter.model.User;
 import org.avally.springreactstarter.payload.request.LoginRequest;
 import org.avally.springreactstarter.payload.request.SignupRequest;
-import org.avally.springreactstarter.payload.response.JwtResponse;
 import org.avally.springreactstarter.payload.response.MessageResponse;
-import org.avally.springreactstarter.repository.RoleRepository;
 import org.avally.springreactstarter.repository.UserRepository;
-import org.avally.springreactstarter.security.jwt.JwtUtils;
-import org.avally.springreactstarter.security.service.UserDetailsImpl;
+import org.avally.springreactstarter.security.JwtTokenProvider;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Map;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    private final PasswordEncoder encoder;
-    private final JwtUtils jwtUtils;
-    private final RoleRepository roleRepository;
-    private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder encoder, JwtUtils jwtUtils) {
-        this.authenticationManager = authenticationManager;
+    public AuthController(UserRepository userRepository, JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.encoder = encoder;
-        this.jwtUtils = jwtUtils;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
-
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(new JwtResponse(jwt,
-                userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                roles));
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest request) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+            User user = userRepository
+                    .findByEmail(request.getEmail())
+                    .orElseThrow(() -> new UsernameNotFoundException("User with an email: " + "doesn't exists"));
+            String token = jwtTokenProvider.createToken(request.getEmail(), user.getRole().name());
+            Map<String, String> response = Map.of(
+                    "accessToken", token,
+                    "username", user.getUsername(),
+                    "email", user.getEmail(),
+                    "role", user.getRole().name()
+            );
+            return ResponseEntity.ok(response);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>(new MessageResponse("invalid email/password combination"), HttpStatus.FORBIDDEN);
+        }
     }
+
+//    todo: add global failure handling for 5** status codes
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+    public ResponseEntity<?> signUp(@RequestBody @Valid SignupRequest signUpRequest) {
         if (userRepository.existsByUsername(signUpRequest.getUsername())) {
             return ResponseEntity
                     .badRequest()
@@ -79,15 +78,16 @@ public class AuthController {
         }
 
         // Create new user's account
-        User user = new User(signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
-                encoder.encode(signUpRequest.getPassword()));
-
-        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-        user.setRoles(Set.of(userRole));
+        User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(),
+                passwordEncoder.encode(signUpRequest.getPassword()), Role.USER, Status.ACTIVE);
         userRepository.save(user);
-
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, null);
+        return ResponseEntity.ok(new MessageResponse("Logged out"));
     }
 }
